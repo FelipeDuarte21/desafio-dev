d4acdd7ca1403e1f346892fdc782d54cf0b3b7e2
Felipe Duarte <felipe15lfde@gmail.com>
2022-04-08 17:42:29 -0300

    Desenvolvendo a classe para extrair dados do arquivo CNAB

---
+++ backend/src/main/java/br/com/luizfelipe/backend/utils/ArquivoUtil.java
@@ -0,0 +1,17 @@
+package br.com.luizfelipe.backend.utils;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ArquivoUtil {
+	
+	public List<String[]> obterDadosAPartirDeArquivo(MultipartFile arquivo){
+		
+		return null; //implementar
+		
+	}
+	
+}
