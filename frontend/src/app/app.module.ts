44cb129517aeee5be20db077aeea7876221059d1
Felipe Duarte <felipe15lfde@gmail.com>
2022-04-09 20:04:02 -0300

    Melhorando o layout com bootstrap e fontawesome

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import ptBr from '@angular/common/locales/pt';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { ListaComponent } from './lista/lista.component';
+import { registerLocaleData } from '@angular/common';
+import { CpfPipe } from './cpf.pipe';
+
+registerLocaleData(ptBr);
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		InputComponent,
-		ListaComponent
+		ListaComponent,
+		CpfPipe
 	],
 	imports: [
 		BrowserModule,
 		HttpClientModule
 	],
-	providers: [],
+	providers: [
+		{
+            provide: LOCALE_ID,
+            useValue: 'pt' 
+        },
+        {
+            provide: DEFAULT_CURRENCY_CODE,
+            useValue: 'BRL'
+        }
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
