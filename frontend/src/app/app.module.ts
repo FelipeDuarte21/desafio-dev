51e876499d069438280c0e6dabad726d2448526c
Felipe Duarte <felipe15lfde@gmail.com>
2022-04-09 00:27:57 -0300

    Declarando os components no modulo principal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { ListaComponent } from './lista/lista.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		InputComponent,
+		ListaComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
